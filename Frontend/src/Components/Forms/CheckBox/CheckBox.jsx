a80969d09f8cdbf4327b173a9ae7e7229e55d10d
Mahmud <jakirmahi20009>
2025-04-28 15:37:21 +0600

    checkbox new functionality

--- Frontend/src/Components/Forms/CheckBox/CheckBox.jsx
+++ Frontend/src/Components/Forms/CheckBox/CheckBox.jsx
@@ -1,33 +1,35 @@
 import styles from "./CheckBox.module.css";
 import Checkbox from "@mui/material/Checkbox";
 import { grey } from "@mui/material/colors";
-const CheckBox = ({ name, setIsChecked, defaultChecked = false }) => {
-  const toggle = () => {
-    setIsChecked((prev) => {
-      return { ...prev, [value]: !prev[value] };
-    });
-  };
+import { useEffect, useState } from "react";
+const CheckBox = ({ name, defaultChecked = false }) => {
+  const [checked, isChecked] = useState(defaultChecked);
   const sx = {
     color: grey[300],
     "&.Mui-checked": {
       color: grey[300],
     },
   };
+  useEffect(() => {
+    isChecked(defaultChecked);
+  }, [defaultChecked]);
   return (
-    defaultChecked && (
-      <div className={styles["checkBox"]}>
-        <Checkbox
-          name={name}
-          defaultChecked={defaultChecked}
-          style={{
-            transform: "scale(1.2)",
-          }}
-          sx={sx}
-        />
+    <div className={styles["checkBox"]}>
+      <Checkbox
+        name={name}
+        // defaultChecked={defaultChecked}
+        onClick={() => {
+          isChecked((prev) => !prev);
+        }}
+        checked={checked}
+        style={{
+          transform: "scale(1.2)",
+        }}
+        sx={sx}
+      />
 
-        <span className={styles["title"]}>{"Activate account"}</span>
-      </div>
-    )
+      <span className={styles["title"]}>{"Activate account"}</span>
+    </div>
   );
 };
 
