6c0cd064925a89adb1bbcabf743af3691af662ab
Mahmud <jakirmahi20009>
2025-06-12 02:54:34 +0600

    bug fix

--- Frontend/src/Pages/SignIn.jsx
+++ Frontend/src/Pages/SignIn.jsx
@@ -1,7 +1,11 @@
 import { useEffect } from "react";
 import SignInForm from "../Components/Forms/SignInForm/SignInForm";
 import { useDispatch } from "react-redux";
-import { IsAuthenticated } from "../Features/UserAuth/UserAuth";
+import {
+  IsAuthenticated,
+  user,
+  isLoading,
+} from "../Features/UserAuth/UserAuth";
 import { Navigate } from "react-router-dom";
 import { useLocation } from "react-router-dom";
 import { useSelector } from "react-redux";
@@ -9,16 +13,18 @@ import { updateNavbar } from "../Features/Navbar";
 const SignIn = () => {
   const location = useLocation();
   const dispatch = useDispatch();
-
   const path = location.state ? location.state.prev : "dashboard";
-  const { user } = useSelector((state) => state.auth);
+  const userState = useSelector(user);
+  const loading = useSelector(isLoading);
+
   useEffect(() => {
     dispatch(updateNavbar(path.split("/")[1]));
-    if (!user) {
+    if (!userState && loading) {
       dispatch(IsAuthenticated());
     }
   }, []);
-  return user ? <Navigate to={path} /> : <SignInForm />;
+
+  return userState ? <Navigate to={path} replace /> : <SignInForm />;
 };
 
 export default SignIn;
