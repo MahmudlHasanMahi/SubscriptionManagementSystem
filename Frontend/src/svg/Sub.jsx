79c21fee45e7e1ccaa07292185249631145aca2a
Mahmud <jakirmahi20009>
2025-06-12 02:54:53 +0600

    svg

---
+++ Frontend/src/svg/Sub.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const Sub = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="30"
+      height="30"
+      viewBox="0 0 30 30"
+      fill="none"
+    >
+      <path
+        d="M29.0332 16.25L20 25.2832L14.0918 19.375L16.25 17.2168L20 20.9668L26.875 14.0918L29.0332 16.25ZM12.7002 17.2998V20.2002H3.5498V17.2998H12.7002ZM17.7002 12.2998V15.2002H3.5498V12.2998H17.7002ZM17.7002 7.2998V10.2002H3.5498V7.2998H17.7002Z"
+        fill="#652324"
+        stroke="#652324"
+        stroke-width="0.4"
+      />
+    </svg>
+  );
+};
+
+export default Sub;
