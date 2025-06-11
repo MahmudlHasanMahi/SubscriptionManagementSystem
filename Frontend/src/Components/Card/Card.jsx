ffcea2510f1df92e43c2f56df51c531cb0111c7f
Mahmud <jakirmahi20009>
2025-06-12 02:47:30 +0600

    extending functionality

--- Frontend/src/Components/Card/Card.jsx
+++ Frontend/src/Components/Card/Card.jsx
@@ -1,9 +1,26 @@
 import React from "react";
 import styles from "./Card.module.css";
 import NumericTextBox from "./NumericTextBox";
-const Card = ({ title1, title2, logo, bg }) => {
+import { useNavigate } from "react-router-dom";
+const Card = ({
+  title1,
+  title2,
+  logo,
+  style,
+  highlight = false,
+  link = null,
+}) => {
+  const navigate = useNavigate();
   return (
-    <div className={styles["card"]} style={{ backgroundColor: bg }}>
+    <div
+      className={`${styles["card"]} ${link && !highlight && styles["hover"]}`}
+      style={{ ...style, outline: highlight ? "solid #FFFFFF" : "none" }}
+      onClick={() => {
+        if (link) {
+          return navigate(link);
+        }
+      }}
+    >
       <NumericTextBox title1={title1} title2={title2} />
       <span>{logo}</span>
     </div>
