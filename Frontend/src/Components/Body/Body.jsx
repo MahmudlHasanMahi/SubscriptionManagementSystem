ffa070a08002ba1790eecd9c7bc4845dec8a07eb
Mahmud <jakirmahi20009>
2025-06-30 12:27:10 +0600

    animation

--- Frontend/src/Components/Body/Body.jsx
+++ Frontend/src/Components/Body/Body.jsx
@@ -1,9 +1,17 @@
 import styles from "./Body.module.css";
+import { AnimatePresence, motion, useIsPresent } from "framer-motion";
 const Body = (props) => {
   return (
-    <div {...props} className={styles["body"]}>
+    <motion.div
+      key="page"
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ delay: 0, duration: 0.3 }}
+      {...props}
+      className={styles["body"]}
+    >
       {props.children}
-    </div>
+    </motion.div>
   );
 };
 
