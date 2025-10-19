306d47ebc8b3b276c8908e7c3c140f563945cb8a
Mahmud <jakirmahi20009>
2025-10-19 19:10:44 +0600

    Celery

---
+++ SubscriptionManagement/celery.py
@@ -0,0 +1,10 @@
+import os 
+from celery import Celery
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SubscriptionManagement.settings')
+
+app = Celery("SubscriptionManagement")
+
+app.config_from_object("django.conf:settings",namespace="CELERY")
+
+app.autodiscover_tasks()
