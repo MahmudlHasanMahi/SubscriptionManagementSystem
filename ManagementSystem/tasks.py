16328ea56322cdcb241faa5eff26f1c3f5ff4157
Mahmud <jakirmahi20009>
2025-10-19 19:09:25 +0600

    Task

---
+++ ManagementSystem/tasks.py
@@ -0,0 +1,30 @@
+from celery import shared_task
+from .models import Subscription,Invoice
+from .Mixins.Time import Time
+from .serlializer import SubscriptionSerializer
+@shared_task
+def scheduled_task():
+    arr = []
+    subs = Subscription.objects.scheduled_subscription()
+    for sub in subs:
+        before = SubscriptionSerializer(sub).data 
+        
+        sub.activate(commit=True,renew=True,activate_plans=True)
+        after = SubscriptionSerializer(sub).data 
+        log = {
+            "before": before,
+            "after":after
+        }
+        arr.append(log) 
+
+    subs = Subscription.objects.due_for_renewal()
+    for sub in subs:
+        # sub.active_subscription_plans(commit=True)
+        sub.renew_subscription(commit=True,generate_invoice=True)
+
+    return arr
+    
+
+@shared_task
+def send_mail():
+    return
