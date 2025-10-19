87eb2f0e63aaf12e420cffe1c2e21b67d846796c
Mahmud <jakirmahi20009>
2025-10-19 19:01:49 +0600

    .

---
+++ Frontend/src/Components/Forms/TextFields/NumericInputField.jsx
@@ -0,0 +1,61 @@
+import React from "react";
+import { useState } from "react";
+import styles from "./TextFields.module.css";
+import { notifyDefault } from "../../../Utils/nofify";
+import { useDebouncedCallback } from "use-debounce";
+import Label from "./Label";
+const NumericInputField = ({
+  type,
+  label,
+  name,
+  required = false,
+  placeholder = "",
+  style,
+  value,
+  icon,
+  editField = false,
+  setinput = null,
+  disabled = false,
+  ref,
+  handleInvalid = null,
+  handleInput = null,
+}) => {
+  const [clipboard, setClipBoard] = useState(null);
+  const [edit, setEdit] = useState(false);
+  const onchange = useDebouncedCallback((value) => {
+    // setinput(value);
+  }, 500);
+
+  return (
+    <div className={styles["textField"]}>
+      <Label label={label} />
+      <div
+        style={{
+          ...style,
+          opacity: !edit && "0.7",
+        }}
+        className={styles["inputField"]}
+      >
+        <input
+          ref={ref}
+          disabled={disabled}
+          onChange={(e) => onchange(e.target.value)}
+          style={{
+            cursor: disabled && "not-allowed",
+            textAlign: !edit && editField && "center",
+          }}
+          type={type}
+          name={name}
+          required={true}
+          placeholder={placeholder}
+          defaultValue={value}
+          readOnly={value && !edit ? true : false}
+          onInvalid={handleInvalid}
+          onInput={handleInput}
+        />
+      </div>
+    </div>
+  );
+};
+
+export default NumericInputField;
