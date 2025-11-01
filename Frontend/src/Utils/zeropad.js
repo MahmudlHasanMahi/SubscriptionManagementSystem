5db5e0201e3edf8ff6c542e63eac266da81f7d36
Mahmud <jakirmahi20009>
2025-11-01 19:38:16 +0600

    Cleanup

--- Frontend/src/Utils/zeropad.js
+++
@@ -1,4 +0,0 @@
-const formattedString = (number) => {
-  return (number || 0).toString().padStart(2, "0");
-};
-export default formattedString;
--- ManagementSystem/process.py
+++
@@ -1,3 +0,0 @@
-from viewflow.workflow.models import Process,models
-from viewflow import this,jsonstore
-# from .models import Subscription
