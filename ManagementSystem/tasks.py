7f486dc62b4fd86526bbdc5c74fe50518920a8fb
Mahmud <jakirmahi20009>
2025-12-04 08:45:36 +0600

    clery tasks

--- ManagementSystem/tasks.py
+++ ManagementSystem/tasks.py
@@ -5,9 +5,11 @@ from .serlializer import SubscriptionSerializer
 @shared_task
 def scheduled_task():
     arr = []
+
     subs = Subscription.objects.scheduled_subscription()  
+
     for sub in subs:
-        before =     SubscriptionSerializer(sub).data 
+        before = SubscriptionSerializer(sub).data 
         
         sub.activate(commit=True,renew=True,activate_plans=True)
         after = SubscriptionSerializer(sub).data 
@@ -20,6 +22,11 @@ def scheduled_task():
     subs = Subscription.objects.due_for_renewal() 
     for sub in subs:
         sub.renew_subscription(commit=True,generate_invoice=True)
+    
+    subs = Subscription.objects.expired()
+
+    for sub in subs:
+        sub.expire(commit=True)
 
     return arr
     
