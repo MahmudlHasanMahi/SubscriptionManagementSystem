701fce2c678963640749af4de46ce5c1d1510649
Mahmud <jakirmahi20009>
2026-02-02 00:45:16 +0600

    mixin

--- User/mixins/TranslatedFieldsMixin.py
+++ User/mixins/TranslatedFieldsMixin.py
@@ -12,13 +12,11 @@ class TranslatedFieldMixin(serializers.Serializer):
         forward_rel = info.forward_relations
         other_field = info.fields
         reverse_rel = info.reverse_relations
-        
         all_fields = {
                     **other_field, 
                     **{k: v.model_field for k, v in forward_rel.items()}, 
                     **{k: v.model_field for k, v in reverse_rel.items()}
                       }
-
         for field in all_fields.values():
             if getattr(field,"name",None) in fields:
                 map[field.name] = field.verbose_name
