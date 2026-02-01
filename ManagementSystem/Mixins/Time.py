be82e7b9e05d723315f977c1defde359d7c23770
Mahmud <jakirmahi20009>
2026-02-02 00:43:51 +0600

    global time

--- ManagementSystem/Mixins/Time.py
+++ ManagementSystem/Mixins/Time.py
@@ -3,5 +3,4 @@ from datetime import timedelta
 class Time:
     @property
     def time_now(self):
-        return timezone.now() + timedelta(days=0)
-        # return timezone.now()
+        return timezone.now() + timedelta(days=3)
