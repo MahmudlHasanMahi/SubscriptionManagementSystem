90fcc2c03e421923247f1349a9025c835f947f28
Mahmud <jakirmahi20009>
2026-02-02 00:49:28 +0600

    invoice endpoints

--- ManagementSystem/urls.py
+++ ManagementSystem/urls.py
@@ -13,10 +13,14 @@ urlpatterns = [
     path("subscription/<int:id>",views.SubscriptionView.as_view()),
     path("subscription/clients",views.ClientView.as_view()),
     path("subscription/unapproved",views.SubscriptionApproval.as_view()),
+    path("subscription/<int:id>/cancel",views.SubscriptionView.as_view(),{"action":"cancel"}),
     path("subscription/<int:pk>/approve",views.SubscriptionApproval.as_view(),{"action":"approve"}),
     path("subscription/<int:pk>/reject",views.SubscriptionApproval.as_view(),{"action":"reject"}),
     path("invoice/<int:pk>",views.Invoice.as_view()),
+    path("invoice/<int:pk>/pay",views.Invoice.as_view(),{"action":"pay"}),
+    path("invoice/<int:pk>/finalize",views.Invoice.as_view(),{"action":"finalize"}),
+    path("invoice",views.Invoice.as_view()),
+    path("invoices",views.InvoiceListView.as_view()),
+    path("currencies/",views.CurrencyView.as_view()),
     path("test/",views.test.as_view()),
-
-
 ]
